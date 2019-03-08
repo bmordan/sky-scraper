e8776dc393e25a0c0311bde0284638472795fb9a
Bernard Mordan <bernard.mordan@whitehat.org.uk>
2019-03-08 17:18:44 +0000

    enables CORS

--- server.js
+++ server.js
@@ -4,5 +4,10 @@ const app = express()
 const PORT = process.env.PORT || 8000
 
 app.use(express.static('public'))
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  next()
+})
 app.get('/', require('./routes/'))
 app.listen(PORT, () => console.log(`listening on port ${PORT} with key ${process.env.BING_IMAGE_SEARCH_API_KEY}`))
